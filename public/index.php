091f076d21de283019b89d15cb2e3b6130245f9b
Leandro A. Dickmann <102850262+LeandroDickmann@users.noreply.github.com>
2023-09-21 20:48:04 -0400

    lienBaseDeDonnées
    La classe destinée à encapsuler la connexion à la base de données a été créée. Test de connexion ok.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
-</head>
-<body>
-    <h1>teste</h1>
-</body>
-</html>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+Database::getConnection();
---
+++ src/config/database.php
@@ -0,0 +1,17 @@
+<?php 
+class Database {
+    public static function getConnection() {
+        $envPath = realpath(dirname(__FILE__). '/../env.ini' );
+        $env = parse_ini_file ($envPath);
+        $conn = new mysqli($env['host'],$env['username'],$env['password'],$env['database'],);
+
+        if($conn-> connect_error) {
+            die("Error: " . $conn->connect_error);
+        }else {
+            die("Test de connexion ok.");
+        }
+
+        return $conn;
+    }
+}
+?>
